42abf227dac00ac80d4371695f6022f26f0567e0
MatthewWaldner <52010927+MatthewWaldner@users.noreply.github.com>
2019-07-20 10:18:23 -0600

    Add files via upload

---
+++ recycle_utils.py
@@ -0,0 +1,81 @@
+#This file contains the utility scripts originating the recycler package that are imported for use in sepsis
+#https://github.com/Shamir-Lab/Recycler
+import numpy as np
+import networkx as nx
+import re
+
+def readfq(fp): # this is a generator function
+    """ # lh3's fast fastX reader:
+        https://github.com/lh3/readfq/blob/master/readfq.py
+    """
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea? yes.
+        if not last: # the first record or a record following a fastq
+            for l in fp: # search for the start of the next record
+                if l[0] in '>@': # fasta/q header line
+                    last = l[:-1] # save this line
+                    break
+        if not last: break
+        name, seqs, last = last[1:].partition(" ")[0], [], None
+        for l in fp: # read the sequence
+            if l[0] in '@+>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+': # this is a fasta record
+            yield name, ''.join(seqs), None # yield a fasta record
+            if not last: break
+        else: # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp: # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq): # have read enough quality
+                    last = None
+                    yield name, seq, ''.join(seqs); # yield a fastq record
+                    break
+            if last: # reach EOF before reading enough quality
+                yield name, seq, None # yield a fasta record instead
+                break
+
+def get_fastg_digraph(fastg_name):
+    """ scans through fastg headers as an adjacency list
+        builds and returns a nx directed graph using adjacencies
+        note: no connections are created between each node and its
+        rc node - we need to take care to maintain these
+    """
+    lines = []
+    fp = open(fastg_name, 'r')
+    for name,seq,qual in readfq(fp):
+        name = re.sub('[:,]'," ", name[:-1])
+        lines.append(name)
+    G = nx.DiGraph()
+    return nx.parse_adjlist(lines, create_using=G)
+
+def rc_node(node):
+    """ gets reverse complement
+        spades node label
+    """
+    if node[-1] == "'": return node[:-1]
+    else: return node + "'"
+
+#takes the coverage from the name of the node in the graph produced by SPAdes
+def get_cov_from_spades_name(name):
+    name_parts = name.split("_")
+    cov = name_parts[5]
+    if cov[-1]=="'": cov=cov[:-1]
+    return float(cov)
+
+def get_fastg_seqs_dict(fastg_name, G):
+    """ returns a dictionary of sequences in graph
+        where node names are keys and sequence strings
+        are values; useful for saving memory when G
+        is a subgraph (e.g., a component)
+    """
+    fp = open(fastg_name, 'r')
+    seqs = {}
+    for name,seq,qual in readfq(fp):
+        name_parts = re.sub('[:,]'," ", name[:-1]).split()
+        node = name_parts[0]
+        seqs[node] = seq
+    return seqs
